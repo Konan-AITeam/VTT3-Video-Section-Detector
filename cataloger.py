3a93e6388bdd84b29f95c2e5bef5b32e8e3427ad
Euncheol-Kang <44823461+Euncheol-Kang@users.noreply.github.com>
2020-11-16 16:51:24 +0900

    Update cataloger.py

--- cataloger.py
+++ cataloger.py
@@ -10,3 +10,38 @@ import JSONImport import *
 class Cataloger:
   def __init__(self, db_data, url, s):
     pass
+
+  def cataloging(self):
+    pass
+  
+  def cut_scene(self):
+    pass
+  
+  def get_shot_info(self):
+    pass
+  
+  def get_video_info(self):
+    pass
+  
+  def calcStep(self):
+    pass
+  
+  def my_round(self):
+    pass
+  
+  def second_to_ms(self):
+    pass
+  
+  def time_to_ms(self):
+    pass
+  
+  def second_to_time(self):
+    pass
+  
+  def extract_all(self):
+    pass
+  
+  def post_data(self):
+    pass
+  
+  
