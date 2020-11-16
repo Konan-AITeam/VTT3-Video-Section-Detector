78e540efaa5f54840756e4efd412e8b955a3876a
Euncheol-Kang <44823461+Euncheol-Kang@users.noreply.github.com>
2020-11-16 16:40:30 +0900

    Create cataloger.py

---
+++ cataloger.py
@@ -0,0 +1,12 @@
+import os
+import copy
+import requests
+
+import shutil import copy2, rmtree
+import datetime import datetime
+import JSONImport import *
+
+
+class Cataloger:
+  def __init__(self, db_data, url, s):
+    pass
